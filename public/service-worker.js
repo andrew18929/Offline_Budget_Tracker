56c7c67f4a48a168cd18e07a0593ca88c1164921
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-12-09 16:14:08 -0600

    coded service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,81 @@
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/dist/bundle.js",
+  "/db.js",
+  "service-worker.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/css/style.css",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  const current = [STATIC_CACHE, RUNTIME_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !current.includes(cacheName));
+      })
+      .then((deletedCaches) => {
+        return Promise.all(
+          deletedCaches.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if (event.request.url.includes("/api/transaction")) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((responseFromCache) => {
+      if (responseFromCache) {
+        return responseFromCache;
+      }
+
+      return caches.open(RUNTIME_CACHE).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
